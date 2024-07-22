1f04d854112f723ea3e5d1ad898860cef0884bc8
undefined-0 <3047423259@qq.com>
2024-07-22 16:38:34 +0800

    新增舵机控制的两种方法

---
+++ 4_servo_control/4_servo_control_v2.py
@@ -0,0 +1,19 @@
+# v2 使用angle方法控制舵机
+
+import time
+from pyb import Servo
+
+s1 = Servo(1) # P7
+s2 = Servo(2) # P8
+
+move_time = 1000 # 1000ms
+
+while(True):
+    # P7连接的舵机
+    s1.angle(90, move_time)
+    time.sleep(10)
+    s1.angle(-90, move_time)
+    time.sleep(10)
+
+    # P8连接的舵机
+
